b8157f5f7d438f83af1193edf294744e03ab3655
Michael albano dias <49378351+AlbanoMike@users.noreply.github.com>
2024-02-06 22:02:45 -0300

    Update Main.java
    ajustes na string sql

--- MySQLData2/src/Aplication/Main.java
+++ MySQLData2/src/Aplication/Main.java
@@ -8,16 +8,18 @@ import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleDateFormat sfd = new SimpleDateFormat("dd//MM/yyyy");
+        SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
         PreparedStatement st = null;
-        try {
+
+      try {
             conn = DB.getConnection();
-            st = conn.prepareStatement("INSERT TO seller (Name, Email, BaseSalary, DepartmentId VALUES "
-            + "?,?,?,?,?");
+            st = conn.prepareStatement("INSERT INTO coursejdbc.seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES"
+            + "(?,?,?,?,?)");
             st.setString(1, "Carl Purple");
             st.setString(2, "Carl@gmail.com");
             st.setDate(3, new java.sql.Date(sfd.parse("22/04/1985").getTime()));
+            st.setDouble(4, 3000);
             st.setInt(5,4);
 
             int rowsaffected = st.executeUpdate();
